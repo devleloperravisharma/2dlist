32e7c85fd073035f0819e0cfad1a06944e052fc5
devleloperravisharma <47600036+devleloperravisharma@users.noreply.github.com>
2024-06-02 12:11:21 -0500

    Add files via upload

---
+++ 2dlist.py
@@ -0,0 +1,21 @@
+#learning how to create a 2d list
+
+matrix = [[134, 1048, 1024], [235, 935, 943], [777, 778, 779]]
+
+#printing a list
+
+print(matrix)
+
+#how to calculate the number of items in a list
+
+num = len(matrix)
+
+print(num)
+
+#printing individual items of a list
+
+print(matrix[0])
+
+#how to print a number of a list in a list
+
+print(matrix[2][1])
